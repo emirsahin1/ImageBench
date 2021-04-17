869e18b3286d9b16d2e693f6ecafd0b903d3c24d
Emir Sahin <emirsah1@live.com>
2021-04-17 01:44:18 -0400

    Cleaner Render process

--- TestTing/ImagePanel.cpp
+++ TestTing/ImagePanel.cpp
@@ -1,31 +1,32 @@
 #include "ImagePanel.h"
 #include "wx/timer.h"
 #include <wx/rawbmp.h>
-
+//TODO: Clean the process of rendering.
+//TODO: Fix Pointers
 wxBEGIN_EVENT_TABLE(ImagePanel, wxPanel)
-	EVT_PAINT(ImagePanel::paintEvent)
-	EVT_SIZE(ImagePanel::onSize)
-	EVT_MOUSEWHEEL(ImagePanel::onScroll)
-	EVT_MOTION(ImagePanel::panHandler)
-	EVT_LEFT_DOWN(ImagePanel::leftDown)
-	EVT_LEFT_UP(ImagePanel::leftUp)
-	EVT_RIGHT_DOWN(ImagePanel::drawAtMouse)
+EVT_PAINT(ImagePanel::paintEvent)
+EVT_SIZE(ImagePanel::onSize)
+EVT_MOUSEWHEEL(ImagePanel::onScroll)
+EVT_MOTION(ImagePanel::panHandler)
+EVT_LEFT_DOWN(ImagePanel::leftDown)
+EVT_LEFT_UP(ImagePanel::leftUp)
+//EVT_RIGHT_DOWN(ImagePanel::drawAtMouse)
 wxEND_EVENT_TABLE()
 
 ImagePanel::ImagePanel(wxWindow* parent) :
-	wxPanel(parent) {
+	wxPanel(parent), isPanning(false), isLoading(false), aspRatio(0), imageOffsetX(0), imageOffsetY(0), 
+	canvw(0), canvh(0), scale(1), imagePosX(0), imagePosY(0), imgh(0), imgw(0), mouseX(0), mouseY(0), panAmountX(0), panAmountY(0)
+{
 	loadingScreen.LoadFile("loadingScreen.png");
 	SetDoubleBuffered(true);
-	isPanning = false; 
-	isLoading = false; 
-	imageOffsetX, imageOffsetY = 0;
-	canvw, canvh, scale = 1;
 }
 
+//Load image using its path. 
 void ImagePanel::loadImage(wxString path) {
 	isLoading = true;
 	Refresh();
 	Update();
+	wxImage image;
 	image.LoadFile(path, wxBITMAP_TYPE_PNG);
 	if (image.IsOk()) {
 		initImageProps();
@@ -34,42 +35,84 @@ void ImagePanel::loadImage(wxString path) {
 	}
 }
 
+//Load RGB/RGBA image using raw data. Converts to bitmap before render.  
 void ImagePanel::loadImage(uint8_t* imageData, int x, int y, int channels) {
 	isLoading = true;
 	Refresh();
 	Update();
-
+	//Convert the raw data to bitmap first.
 	wxBitmap bitmapImage = *RGBAtoBitmap(imageData, x, y);
 	if (bitmapImage.IsOk()) {
-		initBitmapProps(bitmapImage);
 		rendImage = bitmapImage;
-		std::cout << "BITMAPOK"<< " " << bitmapImage.GetWidth();
+		resetTransforms();
+		initBitmapProps(bitmapImage);
+		scaleToRatio(canvw, canvh);
 		Refresh();
 	}
 }
 
+//Load RGB image using raw data. Converts to wxImage then to bitmap. 
 void ImagePanel::loadImage(uint8_t* imageData, int x, int y){
 	isLoading = true;
 	Refresh();
 	Update();
-	image = wxImage(x, y, imageData, true);
+	wxImage image = wxImage(x, y, imageData, true);
 	if (image.IsOk()) {
-		initImageProps();
 		rendImage = (wxBitmap)image;
+		resetTransforms();
+		initImageProps();
+		scaleToRatio(canvw, canvh);
 		Refresh();
 	}
 }
 
+//Load RGBA image using raw data. Converts to wxImage then to bitmap. 
 void ImagePanel::loadImage(uint8_t* imageData, uint8_t* imageAlpha, int x, int y){
 	isLoading = true;
 	Refresh();
 	Update();
-	image = wxImage(x, y, imageData, imageAlpha, true);
+	wxImage image = wxImage(x, y, imageData, imageAlpha, true);
 	if (image.IsOk()) {
-		initImageProps();
 		rendImage = (wxBitmap)image;
+		resetTransforms();
+		initImageProps();
+		scaleToRatio(canvw, canvh);
 		Refresh();
 	}
+}
+
+//Converts raw RGB/RGBA data to a bitmap. 
+wxBitmap* ImagePanel::RGBAtoBitmap(uint8_t* rgba, int w, int h)
+{
+	wxBitmap* bitmap = new wxBitmap(w, h, wxBITMAP_SCREEN_DEPTH);
+	if (!bitmap->Ok()) {
+		delete bitmap;
+		return NULL;
+	}
+
+	wxAlphaPixelData bmdata(*bitmap);
+	if (bmdata == NULL) {
+		wxLogDebug(wxT("getBitmap() failed"));
+		delete bitmap;
+		return NULL;
+	}
+
+	wxAlphaPixelData::Iterator dst(bmdata);
+
+	for (int y = 0; y < h; y++) {
+		dst.MoveTo(bmdata, 0, y);
+		for (int x = 0; x < w; x++) {
+			// wxBitmap contains rgb values pre-multiplied with alpha
+			unsigned char a = rgba[3];
+			dst.Red() = rgba[0] * a / 255;
+			dst.Green() = rgba[1] * a / 255;
+			dst.Blue() = rgba[2] * a / 255;
+			dst.Alpha() = a;
+			dst++;
+			rgba += 4;
+		}
+	}
+	return bitmap;
 }
 
 void ImagePanel::setLoading(bool isLoading){
@@ -77,41 +120,58 @@ void ImagePanel::setLoading(bool isLoading){
 }
 
 void ImagePanel::initImageProps(){
+	imgw = rendImage.GetWidth();
+	imgh = rendImage.GetHeight();
+	aspRatio = imgw / imgh;
+}
+
+void ImagePanel::resetTransforms() {
 	scale = 1;
 	imageOffsetX = 0;
 	imageOffsetY = 0;
-	imgw = image.GetWidth();
-	imgh = image.GetHeight();
-	aspRatio = imgw / imgh;
-	//Fits the image w and h to the desired ratio before render. 
-	fitImageToRatio(canvw, canvh);
-	//scale = imgw / image.GetWidth();
-	updateImage();
 }
 
 void ImagePanel::initBitmapProps(wxBitmap bitmap) {
-	scale = 1;
-	imageOffsetX = 0;
-	imageOffsetY = 0;
 	imgw = bitmap.GetWidth();
 	imgh = bitmap.GetHeight();
 	aspRatio = imgw / imgh;
-	//Fits the image w and h to the desired ratio before render. 
-	fitImageToRatio(canvw, canvh);
-	//scale = imgw / image.GetWidth();
+}
+
+void ImagePanel::scaleToRatio(int contw, int conth) {
+	/*if (image.IsOk()) {
+		float newAspRatio = (float)contw / conth;
+		if (imgh < imgw) {
+			imgh = contw / aspRatio;
+			imgw = contw;
+		}
+		else {
+			imgh = conth;
+			imgw = conth * aspRatio;
+		}
+	}*/
+	float newAspRatio = (float)contw / conth;
+	float newImgw, newImgh;
+	if (imgh < imgw && imgh > conth) {
+		newImgh = contw / aspRatio;
+		newImgw = contw;
+		scale = newImgh / imgh;
+	}
+	else {
+		newImgh = conth;
+		newImgw = conth * aspRatio;
+		scale = newImgh / imgh;
+	}
 }
 
 //Does not render, but prepares to render. 
 void ImagePanel::paintEvent(wxPaintEvent& evt) {
 	wxPaintDC dc(this);
 	dc.GetSize(&canvw, &canvh);
+	//Calculates the images offset based on scale, centers it. 
 	imagePosX = (canvw / scale - imgw) / 2 + (panAmountX + imageOffsetX) / scale;
 	imagePosY = (canvh / scale - imgh) / 2 + (panAmountY + imageOffsetY) / scale;
 
 	if (rendImage.IsOk()) {
-		/*if (abs((rendImage.GetWidth() - imgw)) > 1 || abs((rendImage.GetHeight() - imgh)) > 1) {
-			rendImage = image.Scale(imgw, imgh, wxIMAGE_QUALITY_NORMAL);
-		}*/
 		render(dc);
 	}
 	if (isLoading) {
@@ -138,32 +198,6 @@ void ImagePanel::renderLoading(wxDC& dc) {
 		std::cout << isLoading << "ADAWDADAWD\n";
 }
 
-void ImagePanel::fitImageToRatio(int contw, int conth) {
-	/*if (image.IsOk()) {
-		float newAspRatio = (float)contw / conth;
-		if (imgh < imgw) {
-			imgh = contw / aspRatio;
-			imgw = contw;
-		}
-		else {
-			imgh = conth;
-			imgw = conth * aspRatio;
-		}
-	}*/
-
-	float newAspRatio = (float)contw / conth;
-	float newImgw, newImgh;
-	if (imgh < imgw && imgh > conth) {
-		newImgh = contw / aspRatio;
-		newImgw = contw;
-		scale = newImgh / imgh;
-	}
-	else {
-		newImgh = conth;
-		newImgw = conth * aspRatio;
-		scale = newImgh / imgh;
-	}
-}
 	
 void ImagePanel::onScroll(wxMouseEvent& evt){
 	if (evt.GetWheelRotation() > 0) {
@@ -209,26 +243,21 @@ void ImagePanel::rightDown(wxMouseEvent& evt) {
 	std::cout << pixelLoc.x << "\n" << pixelLoc.y << "\n";
 }
 
-void ImagePanel::drawAtMouse(wxMouseEvent& evt) {
-	wxPoint pixelLoc = evt.GetPosition();;
-	double x = (double)(pixelLoc.x) / (canvw);
-	double y = (double)(pixelLoc.y) / (canvh);
-	double newyVal = (pixelLoc.y - imagePosY) * scale;
-	std::cout << "\n\nMouse Location:  X: " << pixelLoc.x << " Y: " << pixelLoc.y;
-	std::cout << "\nNormalized Values: " << " X Value: " << x << " Y Value: " << y;
-	std::cout << "\nImage Positions: imagePosX: " << imagePosX * scale << " imagePosY: " << imagePosY * scale;
-	std::cout << "\nCalculated Pixel Coordinate: X: " << imgw / 2 << "  Y: " << newyVal;
-	if (pixelLoc.y >= imagePosY) {
-		image.SetRGB(50, newyVal, 0, 255, 0);
-	}
-	updateImage();
-	Refresh();
-}
-
-void ImagePanel::updateImage(){
-	//rendImage = (wxBitmap)image.Scale(imgw, imgh, wxIMAGE_QUALITY_BILINEAR);
-	//rendImage = (wxBitmap)image;
-}
+//void ImagePanel::drawAtMouse(wxMouseEvent& evt) {
+//	wxPoint pixelLoc = evt.GetPosition();;
+//	double x = (double)(pixelLoc.x) / (canvw);
+//	double y = (double)(pixelLoc.y) / (canvh);
+//	double newyVal = (pixelLoc.y - imagePosY) * scale;
+//	std::cout << "\n\nMouse Location:  X: " << pixelLoc.x << " Y: " << pixelLoc.y;
+//	std::cout << "\nNormalized Values: " << " X Value: " << x << " Y Value: " << y;
+//	std::cout << "\nImage Positions: imagePosX: " << imagePosX * scale << " imagePosY: " << imagePosY * scale;
+//	std::cout << "\nCalculated Pixel Coordinate: X: " << imgw / 2 << "  Y: " << newyVal;
+//	if (pixelLoc.y >= imagePosY) {
+//		rendImage.SetRGB(50, newyVal, 0, 255, 0);
+//	}
+//	updateImage();
+//	Refresh();
+//}
 
 void ImagePanel::onSize(wxSizeEvent& evt) {
 	Refresh();
@@ -239,49 +268,3 @@ void ImagePanel::bgErase(wxEraseEvent& evt) {
 
 }
 
-wxBitmap* ImagePanel::RGBAtoBitmap(uint8_t* rgba, int w, int h)
-{
-	wxBitmap* bitmap = new wxBitmap(w, h, wxBITMAP_SCREEN_DEPTH);
-	if (!bitmap->Ok()) {
-		delete bitmap;
-		return NULL;
-	}
-
-	wxAlphaPixelData bmdata(*bitmap);
-	if (bmdata == NULL) {
-		wxLogDebug(wxT("getBitmap() failed"));
-		delete bitmap;
-		return NULL;
-	}
-
-	wxAlphaPixelData::Iterator dst(bmdata);
-
-	//for (int y = 0; y < h; y++) {
-	//	dst.MoveTo(bmdata, 0, y);
-	//	for (int x = 0; x < w; x++) {
-	//		// wxBitmap contains rgb values pre-multiplied with alpha
-	//		unsigned char a = rgba[3];
-	//		dst.Red() = rgba[0] * a / 255;
-	//		dst.Green() = rgba[1] * a / 255;
-	//		dst.Blue() = rgba[2] * a / 255;
-	//		dst.Alpha() = a;
-	//		dst++;
-	//		rgba += 4;
-	//	}
-	//}
-
-	for (int y = 0; y < h; y++) {
-		dst.MoveTo(bmdata, 0, y);
-		for (int x = 0; x < w; x++) {
-			// wxBitmap contains rgb values pre-multiplied with alpha
-			unsigned char a = rgba[3];
-			dst.Red() = rgba[0];
-			dst.Green() = rgba[1];
-			dst.Blue() = rgba[2];
-			dst.Alpha() = a;
-			dst++;
-			rgba += 4;
-		}
-	}
-	return bitmap;
-}
--- TestTing/ImagePanel.h
+++ TestTing/ImagePanel.h
@@ -10,26 +10,25 @@ private:
 	float mouseX, mouseY, panAmountX, panAmountY;
 	bool isPanning, isLoading;
 	wxCursor panCursor;
-	wxImage image;
 	wxImage loadingScreen;
 	wxBitmap rendImage;
 private:
 	void onScroll(wxMouseEvent& evt);
-	void fitImageToRatio(int neww, int newh);
+	void scaleToRatio(int neww, int newh);
 	void panHandler(wxMouseEvent& evt);
 	void leftDown(wxMouseEvent& evt);
 	void leftUp(wxMouseEvent& evt);
 	void rightDown(wxMouseEvent& evt);
 	void rightUp(wxMouseEvent& evt);
 	void drawAtMouse(wxMouseEvent& evt);
-	void updateImage();
 	void render(wxDC& dc);
 	void renderLoading(wxDC& dc);
 	void bgErase(wxEraseEvent& evt);
 	void onSize(wxSizeEvent& evt);
 	void initImageProps();
 	void initBitmapProps(wxBitmap bitmap);
-	wxBitmap* RGBAtoBitmap(unsigned char* rgba, int w, int h);
+	void resetTransforms();
+	wxBitmap* RGBAtoBitmap(uint8_t* rgba, int w, int h);
 
 public:
 	ImagePanel(wxWindow* parent);
